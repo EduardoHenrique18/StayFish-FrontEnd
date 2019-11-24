d5256024fad6e05718a14ff6e8c0c1384fd0fbc5
edv610 <50935061+edv610@users.noreply.github.com>
2019-11-24 18:47:44 -0300

    Update

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem/*, Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter*/  } from 'reactstrap';
 
 
 export default class Dashboard extends Component {
@@ -21,17 +21,19 @@ export default class Dashboard extends Component {
     render() {
         return (
             <div className= "wrapper">
-                <ButtonDropdown direction="up" isOpen={this.state.dropdownOpen} toggle={this.toggle} className= "clicar">
-                <DropdownToggle caret color="primary">
-                    +
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem>Pagamento</DropdownItem>
-                    <DropdownItem>Fatura</DropdownItem>
-                </DropdownMenu>
-                </ButtonDropdown>
+                               
+                    <ButtonDropdown direction="up" isOpen={this.state.dropdownOpen} toggle={this.toggle} className= "clicar">
+                    <DropdownToggle caret color="primary">
+                        +
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem block onClick={this.toggleModal}>Pagamento</DropdownItem>
+                        <DropdownItem>Fatura</DropdownItem>
+                    </DropdownMenu>
+                    </ButtonDropdown>
 
-                
+
+
             </div>
             
         );
