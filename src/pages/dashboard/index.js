4e9a532d45a838a2fcaf847e9588736ab502bb1c
edv610 <50935061+edv610@users.noreply.github.com>
2019-12-06 15:03:24 -0300

    buttons

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -28,11 +28,81 @@ export default class Dashboard extends Component {
                     </DropdownToggle>
                     <DropdownMenu>
                         <DropdownItem block onClick={this.toggleModal}>Pagamento</DropdownItem>
-                        <DropdownItem>Fatura</DropdownItem>
+                        <DropdownItem block onClick={this.toggleModal2}>=Fatura</DropdownItem>
                     </DropdownMenu>
                     </ButtonDropdown>
+                    <Modal isOpen={this.state.modalIsOpen}>
+                    <ModalHeader toggle={this.toggleModal.bind(this)}>
+                        Insira o Pagamento
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="description">Descrição</Label>
+                                <Input type="text" id="descricao" onChange={e => this.description = e.target.value} placeholder="Informe a descrição" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="value">Valor</Label>
+                                <Input type="text" id="valor" onChange={e => this.value = e.target.value} placeholder="Informe o valor" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="paymentDate">Data de nascimento</Label>
+                                <Input type="date" id="dtPagamento" onChange={e => this.paymentDate = e.target.value} placeholder="Informe a data do Pagamento" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="status">Status</Label>
+                                <Input type="text" id="status" onChange={e => this.status = e.target.value} placeholder="Informe o  status" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="note">Observação</Label>
+                                <Input type="text" id="observacao" onChange={e => this.note = e.target.value} placeholder="Informe sua Observação" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="category">Observação</Label>
+                                <Input type="text" id="categoria" onChange={e => this.category = e.target.value} placeholder="Informe a categoria" />
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={this.toggleModal.bind(this)}>Cancelar</Button>
+                        <Button color="primary" onClick={this.createPayment}>Cadastrar Pagamento</Button>
+                    </ModalFooter>
+                </Modal>
 
 
+                <Modal isOpen={this.state.modal2IsOpen}>
+                    <ModalHeader toggle={this.toggleModal2.bind(this)}>
+                        Insira a Fatura
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="description">Descrição</Label>
+                                <Input type="text" id="descricao" onChange={e => this.description = e.target.value} placeholder="Informe a descrição" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="value">Valor</Label>
+                                <Input type="text" id="valor" onChange={e => this.value = e.target.value} placeholder="Informe o valor" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="invoiceDate">Data de nascimento</Label>
+                                <Input type="date" id="dtPagamento" onChange={e => this.invoiceDate = e.target.value} placeholder="Informe a data do Pagamento" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="note">Observação</Label>
+                                <Input type="text" id="observacao" onChange={e => this.note = e.target.value} placeholder="Informe sua Observação" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="category">Observação</Label>
+                                <Input type="text" id="categoria" onChange={e => this.category = e.target.value} placeholder="Informe a categoria" />
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={this.toggleModal2.bind(this)}>Cancelar</Button>
+                        <Button color="primary" onClick={this.createInvoice}>Cadastrar Fatura</Button>
+                    </ModalFooter>
+                </Modal>
 
             </div>
             
